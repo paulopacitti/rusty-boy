480d08e096cfd80ccef1352723422b52c6aa176a
paulopacitti <ppacitti@outlook.com>
2023-01-22 14:42:53 -0300

    Add clock cycles for each instruction implemented

--- core/src/cpu.rs
+++ core/src/cpu.rs
@@ -16,226 +16,493 @@ impl CPU {
         }
     }
 
-    fn execute(&mut self, op: u8) {
+    /// Decode op code and execute instruction. Returns how many clocks were necessary to run the instruction.
+    fn execute(&mut self, op: u8) -> u32 {
         match op {
-            0x00 => self.nop(),
+            0x00 => {
+                self.nop();
+                1
+            }
             0x03 => {
                 let result = self.inc16(self.registers.bc());
                 self.registers.set_bc(result);
+                1
             }
-            0x04 => self.registers.b = self.inc(self.registers.b),
-            0x05 => self.registers.b = self.dec(self.registers.b),
-            0x09 => self.add16_hl(self.registers.bc()),
+            0x04 => {
+                self.registers.b = self.inc(self.registers.b);
+                1
+            }
+            0x05 => {
+                self.registers.b = self.dec(self.registers.b);
+                1
+            }
+            0x09 => {
+                self.add16_hl(self.registers.bc());
+                1
+            }
             0x0B => {
                 let result = self.dec16(self.registers.bc());
                 self.registers.set_bc(result);
+                1
             }
-            0x0C => self.registers.c = self.inc(self.registers.c),
-            0x0D => self.registers.c = self.dec(self.registers.c),
+            0x0C => {
+                self.registers.c = self.inc(self.registers.c);
+                1
+            }
+            0x0D => {
+                self.registers.c = self.dec(self.registers.c);
+                1
+            }
             0x13 => {
                 let result = self.inc16(self.registers.de());
                 self.registers.set_de(result);
+                1
             }
-            0x14 => self.registers.d = self.inc(self.registers.d),
-            0x15 => self.registers.d = self.dec(self.registers.d),
-            0x19 => self.add16_hl(self.registers.de()),
+            0x14 => {
+                self.registers.d = self.inc(self.registers.d);
+                1
+            }
+            0x15 => {
+                self.registers.d = self.dec(self.registers.d);
+                1
+            }
+            0x19 => {
+                self.add16_hl(self.registers.de());
+                1
+            }
             0x1B => {
                 let result = self.dec16(self.registers.de());
                 self.registers.set_de(result);
+                1
             }
-            0x1C => self.registers.e = self.inc(self.registers.e),
-            0x1D => self.registers.e = self.dec(self.registers.e),
+            0x1C => {
+                self.registers.e = self.inc(self.registers.e);
+                1
+            }
+            0x1D => {
+                self.registers.e = self.dec(self.registers.e);
+                1
+            }
             0x23 => {
                 let result = self.inc16(self.registers.hl());
                 self.registers.set_hl(result);
+                1
             }
-            0x24 => self.registers.h = self.inc(self.registers.h),
-            0x25 => self.registers.h = self.dec(self.registers.h),
-            0x29 => self.add16_hl(self.registers.hl()),
+            0x24 => {
+                self.registers.h = self.inc(self.registers.h);
+                1
+            }
+            0x25 => {
+                self.registers.h = self.dec(self.registers.h);
+                1
+            }
+            0x29 => {
+                self.add16_hl(self.registers.hl());
+                1
+            }
             0x2B => {
                 let result = self.dec16(self.registers.hl());
                 self.registers.set_hl(result);
+                1
             }
-            0x2C => self.registers.l = self.inc(self.registers.l),
-            0x2D => self.registers.l = self.dec(self.registers.l),
-            0x2F => self.cpl(),
+            0x2C => {
+                self.registers.l = self.inc(self.registers.l);
+                1
+            }
+            0x2D => {
+                self.registers.l = self.dec(self.registers.l);
+                1
+            }
+            0x2F => {
+                self.cpl();
+                1
+            }
             0x33 => {
                 let result = self.inc16(self.registers.sp);
                 self.registers.sp = result;
+                1
             }
             0x34 => {
                 let address = self.registers.hl();
                 let result = self.inc(self.mmu.read_byte(address));
                 self.mmu.write_byte(address, result);
+                1
             }
             0x35 => {
                 let address = self.registers.hl();
                 let result = self.dec(self.mmu.read_byte(address));
                 self.mmu.write_byte(address, result);
+                1
             }
-            0x39 => self.add16_hl(self.registers.sp),
+            0x39 => {
+                self.add16_hl(self.registers.sp);
+                1
+            }
             0x3B => {
                 let result = self.dec16(self.registers.sp);
                 self.registers.sp = result;
+                1
             }
-            0x3C => self.registers.a = self.inc(self.registers.a),
-            0x3D => self.registers.a = self.dec(self.registers.a),
-            0x3F => self.ccf(),
-            0x80 => self.add(self.registers.b),
-            0x81 => self.add(self.registers.c),
-            0x82 => self.add(self.registers.d),
-            0x83 => self.add(self.registers.e),
-            0x84 => self.add(self.registers.h),
-            0x85 => self.add(self.registers.l),
+            0x3C => {
+                self.registers.a = self.inc(self.registers.a);
+                1
+            }
+            0x3D => {
+                self.registers.a = self.dec(self.registers.a);
+                1
+            }
+            0x3F => {
+                self.ccf();
+                1
+            }
+            0x80 => {
+                self.add(self.registers.b);
+                1
+            }
+            0x81 => {
+                self.add(self.registers.c);
+                1
+            }
+            0x82 => {
+                self.add(self.registers.d);
+                1
+            }
+            0x83 => {
+                self.add(self.registers.e);
+                1
+            }
+            0x84 => {
+                self.add(self.registers.h);
+                1
+            }
+            0x85 => {
+                self.add(self.registers.l);
+                1
+            }
             0x86 => {
                 let address = self.registers.hl();
                 self.add(self.mmu.read_byte(address));
+                1
             }
-            0x87 => self.add(self.registers.a),
-            0x88 => self.adc(self.registers.b),
-            0x89 => self.adc(self.registers.c),
-            0x8A => self.adc(self.registers.d),
-            0x8B => self.adc(self.registers.e),
-            0x8C => self.adc(self.registers.h),
-            0x8D => self.adc(self.registers.l),
+            0x87 => {
+                self.add(self.registers.a);
+                1
+            }
+            0x88 => {
+                self.adc(self.registers.b);
+                1
+            }
+            0x89 => {
+                self.adc(self.registers.c);
+                1
+            }
+            0x8A => {
+                self.adc(self.registers.d);
+                1
+            }
+            0x8B => {
+                self.adc(self.registers.e);
+                1
+            }
+            0x8C => {
+                self.adc(self.registers.h);
+                1
+            }
+            0x8D => {
+                self.adc(self.registers.l);
+                1
+            }
             0x8E => {
                 let address = self.registers.hl();
                 self.adc(self.mmu.read_byte(address));
+                1
             }
-            0x8F => self.adc(self.registers.a),
-            0x90 => self.sub(self.registers.b),
-            0x91 => self.sub(self.registers.c),
-            0x92 => self.sub(self.registers.d),
-            0x93 => self.sub(self.registers.e),
-            0x94 => self.sub(self.registers.h),
-            0x95 => self.sub(self.registers.l),
+            0x8F => {
+                self.adc(self.registers.a);
+                1
+            }
+            0x90 => {
+                self.sub(self.registers.b);
+                1
+            }
+            0x91 => {
+                self.sub(self.registers.c);
+                1
+            }
+            0x92 => {
+                self.sub(self.registers.d);
+                1
+            }
+            0x93 => {
+                self.sub(self.registers.e);
+                1
+            }
+            0x94 => {
+                self.sub(self.registers.h);
+                1
+            }
+            0x95 => {
+                self.sub(self.registers.l);
+                1
+            }
             0x96 => {
                 let address = self.registers.hl();
                 self.sub(self.mmu.read_byte(address));
+                1
             }
-            0x97 => self.sub(self.registers.a),
-            0x98 => self.sub(self.registers.b),
-            0x99 => self.sub(self.registers.c),
-            0x9A => self.sub(self.registers.d),
-            0x9B => self.sub(self.registers.e),
-            0x9C => self.sub(self.registers.h),
-            0x9D => self.sub(self.registers.l),
+            0x97 => {
+                self.sub(self.registers.a);
+                1
+            }
+            0x98 => {
+                self.sub(self.registers.b);
+                1
+            }
+            0x99 => {
+                self.sub(self.registers.c);
+                1
+            }
+            0x9A => {
+                self.sub(self.registers.d);
+                1
+            }
+            0x9B => {
+                self.sub(self.registers.e);
+                1
+            }
+            0x9C => {
+                self.sub(self.registers.h);
+                1
+            }
+            0x9D => {
+                self.sub(self.registers.l);
+                1
+            }
             0x9E => {
                 let address = self.registers.hl();
                 self.sub(self.mmu.read_byte(address));
+                1
             }
-            0x9F => self.sub(self.registers.a),
-            0xA0 => self.and(self.registers.b),
-            0xA1 => self.and(self.registers.c),
-            0xA2 => self.and(self.registers.d),
-            0xA3 => self.and(self.registers.e),
-            0xA4 => self.and(self.registers.h),
-            0xA5 => self.and(self.registers.l),
+            0x9F => {
+                self.sub(self.registers.a);
+                1
+            }
+            0xA0 => {
+                self.and(self.registers.b);
+                1
+            }
+            0xA1 => {
+                self.and(self.registers.c);
+                1
+            }
+            0xA2 => {
+                self.and(self.registers.d);
+                1
+            }
+            0xA3 => {
+                self.and(self.registers.e);
+                1
+            }
+            0xA4 => {
+                self.and(self.registers.h);
+                1
+            }
+            0xA5 => {
+                self.and(self.registers.l);
+                1
+            }
             0xA6 => {
                 let address = self.registers.hl();
                 self.and(self.mmu.read_byte(address));
+                1
             }
-            0xA7 => self.and(self.registers.a),
-            0xA8 => self.xor(self.registers.b),
-            0xA9 => self.xor(self.registers.c),
-            0xAA => self.xor(self.registers.d),
-            0xAB => self.xor(self.registers.e),
-            0xAC => self.xor(self.registers.h),
-            0xAD => self.xor(self.registers.l),
+            0xA7 => {
+                self.and(self.registers.a);
+                1
+            }
+            0xA8 => {
+                self.xor(self.registers.b);
+                1
+            }
+            0xA9 => {
+                self.xor(self.registers.c);
+                1
+            }
+            0xAA => {
+                self.xor(self.registers.d);
+                1
+            }
+            0xAB => {
+                self.xor(self.registers.e);
+                1
+            }
+            0xAC => {
+                self.xor(self.registers.h);
+                1
+            }
+            0xAD => {
+                self.xor(self.registers.l);
+                1
+            }
             0xAE => {
                 let address = self.registers.hl();
                 self.xor(self.mmu.read_byte(address));
+                1
             }
-            0xAF => self.xor(self.registers.a),
-            0xB0 => self.or(self.registers.b),
-            0xB1 => self.or(self.registers.c),
-            0xB2 => self.or(self.registers.d),
-            0xB3 => self.or(self.registers.e),
-            0xB4 => self.or(self.registers.h),
-            0xB5 => self.or(self.registers.l),
+            0xAF => {
+                self.xor(self.registers.a);
+                1
+            }
+            0xB0 => {
+                self.or(self.registers.b);
+                1
+            }
+            0xB1 => {
+                self.or(self.registers.c);
+                1
+            }
+            0xB2 => {
+                self.or(self.registers.d);
+                1
+            }
+            0xB3 => {
+                self.or(self.registers.e);
+                1
+            }
+            0xB4 => {
+                self.or(self.registers.h);
+                1
+            }
+            0xB5 => {
+                self.or(self.registers.l);
+                1
+            }
             0xB6 => {
                 let address = self.registers.hl();
                 self.or(self.mmu.read_byte(address));
+                1
             }
-            0xB7 => self.or(self.registers.a),
-            0xB8 => self.cp(self.registers.b),
-            0xB9 => self.cp(self.registers.c),
-            0xBA => self.cp(self.registers.d),
-            0xBB => self.cp(self.registers.e),
-            0xBC => self.cp(self.registers.h),
-            0xBD => self.cp(self.registers.l),
+            0xB7 => {
+                self.or(self.registers.a);
+                1
+            }
+            0xB8 => {
+                self.cp(self.registers.b);
+                1
+            }
+            0xB9 => {
+                self.cp(self.registers.c);
+                1
+            }
+            0xBA => {
+                self.cp(self.registers.d);
+                1
+            }
+            0xBB => {
+                self.cp(self.registers.e);
+                1
+            }
+            0xBC => {
+                self.cp(self.registers.h);
+                1
+            }
+            0xBD => {
+                self.cp(self.registers.l);
+                1
+            }
             0xBE => {
                 let address = self.registers.hl();
                 self.cp(self.mmu.read_byte(address));
+                1
             }
-            0xBF => self.cp(self.registers.a),
+            0xBF => {
+                self.cp(self.registers.a);
+                1
+            }
             0xC1 => {
                 let value = self.pop();
                 self.registers.set_bc(value);
+                1
             }
             0xC5 => {
                 let value = self.registers.bc();
                 self.push(value);
+                1
             }
             0xC6 => {
                 let value = self.fetch_byte();
                 self.add(value);
+                2
             }
             0xCE => {
                 let value = self.fetch_byte();
                 self.adc(value);
+                2
             }
             0xD1 => {
                 let value = self.pop();
                 self.registers.set_de(value);
+                1
             }
             0xD5 => {
                 let value = self.registers.de();
                 self.push(value);
+                1
             }
             0xD6 => {
                 let value = self.fetch_byte();
                 self.sub(value);
+                2
             }
             0xDE => {
                 let value = self.fetch_byte();
                 self.sbc(value);
+                2
             }
             0xE1 => {
                 let value = self.pop();
                 self.registers.set_hl(value);
+                1
             }
             0xE5 => {
                 let value = self.registers.hl();
                 self.push(value);
+                1
             }
             0xE6 => {
                 let value = self.fetch_byte();
                 self.and(value);
+                2
             }
             0xE8 => {
                 let value = self.fetch_byte();
                 self.add16_sp(value);
+                2
             }
             0xEE => {
                 let value = self.fetch_byte();
                 self.xor(value);
+                2
             }
             0xF1 => {
                 let value = self.pop();
                 self.registers.set_af(value);
+                1
             }
             0xF5 => {
                 let value = self.registers.af();
                 self.push(value);
+                1
             }
             0xF6 => {
                 let value = self.fetch_byte();
                 self.or(value);
+                2
             }
             0xFE => {
                 let value = self.fetch_byte();
                 self.cp(value);
+                2
             }
 
             _ => unimplemented!("Unkown instruction found for: 0x{:x}", op),
